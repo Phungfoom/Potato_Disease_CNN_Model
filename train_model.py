bf2b860767094a2f90c52be9ecf77c38e4950c2b
Phung <phung.k.tran00@gmail.com>
2026-02-02 11:56:43 -0600

    train/validation split

--- train_model.py
+++ train_model.py
@@ -1,8 +1,34 @@
 import os
+import tensorflow as tf
+import matplotlib.pyplot as plt 
 
-def main():
-    print("Potato Disease CNN Project initialized!")
-    print(f"Current Working Directory: {os.getcwd()}")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+dataset_dir = os.path.join(script_dir, 'hot_potato')
 
-if __name__ == "__main__":
-    main()
+batch_size = 32
+img_size = (224, 224)
+seed = 123
+
+print(f"loading data {dataset_dir}")
+
+# Training data (70%)
+train_spud = tf.keras.utils.image_dataset_from_directory(
+    dataset_dir,
+    validation_split = 0.3,
+    subset = "training",
+    seed = 123,
+    image_size = (224, 224),
+    batch_size = 32,
+    shuffle = True  
+)
+
+# Validation data (30%)
+val_spud = tf.keras.utils.image_dataset_from_directory(
+    dataset_dir,
+    validation_split = 0.3,
+    subset = 'validation',
+    seed = 123
+    image_size = (224, 224),
+    batch_size = 32,
+    shuffle = False
+)
