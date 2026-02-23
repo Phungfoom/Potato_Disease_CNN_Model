948b8488314a0969cc1655ef9c6e50ce0c158720
Phung <phung.k.tran00@gmail.com>
2026-02-23 17:53:25 -0600

    Outline for Sobel Edge

--- build_sobel_edge_model.py
+++ build_sobel_edge_model.py
@@ -1,7 +1,28 @@
-# 2. Sobel Edge 
-import os 
-import numpy as np
 import tensorflow as tf
-import cv2
-import matplotlib.pyplot as plt
+    # tensorflow sobel edge detector 
+
+        # vertical and horizontal edges
+
+        # Magnitude = sqrt(G_x^2 + G_y^2)
+        
+    # model input layers (3x3)
+
+        # low 
+        # convolution layers (activation)
+        
+        # pooling layers 
+
+        # middle
+        # convolution layers (activation)
+        
+        # pooling layers 
+
+        # high
+        # convolution layers (activation)
+        
+        # pooling layers 
+    
+        # connection layers (classifier)
+
+        # outputs
 
