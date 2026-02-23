c64289d0480629bd64fcad4b35dcb39650051828
Phung <phung.k.tran00@gmail.com>
2026-02-23 17:56:30 -0600

    outline for each disease

---
+++ which_classifier.py
@@ -0,0 +1,27 @@
+# Late Blight 
+
+# Early Blight
+
+# Mosiac Virus 
+
+# PLVR Virus
+
+# Blackleg
+
+# Blackspot Bruising 
+
+# Brown Spot 
+
+# Common Scab
+
+# Cracking Type
+
+# Dry Rot 
+
+# Pink Rot
+
+# Pest(Aphids)
+
+# PSTVD
+
+# Soft Rot
